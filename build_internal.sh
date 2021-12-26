6ae1cbfab2520bdd4f2cb36741f7474f027b3e54
synoniem@hotmail.com <synoniem@hotmail.com>
2021-12-26 15:12:23 +0100

    Fixed typo's. Added multiarch support

--- build_internal.sh
+++ build_internal.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # Build script for s6-overlay with processor architecture detection
 apkArch=`dpkg --print-architecture`
+if [ $apkArch = "arm64" ]
+then
+    apkArch="aarch64"
+fi
+if [ $apkArch = "armel" ]
+then
+    apkArch="armhf"
+fi
 mkdir /root/tmp
 wget https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-$apkArch-installer -P /root/tmp
 chmod +x /root/tmp/s6-overlay-$apkArch-installer && /root/tmp/s6-overlay-$apkArch-installer / 
