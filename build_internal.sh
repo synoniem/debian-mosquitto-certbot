8c4395c54c081a7a0e7602331c85b87c2236f6c2
synoniem@hotmail.com <synoniem@hotmail.com>
2022-08-26 23:48:51 +0200

    Added i386/x86 archictecture to build_internal.sh

--- build_internal.sh
+++ build_internal.sh
@@ -9,6 +9,11 @@ if [ $apkArch = "armel" ]
 then
     apkArch="armhf"
 fi
+if [ $apkArch = "i386" ]
+then
+    apkArch="x86"
+fi
+
 mkdir /root/tmp
 wget https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-$apkArch-installer -P /root/tmp
 chmod +x /root/tmp/s6-overlay-$apkArch-installer && /root/tmp/s6-overlay-$apkArch-installer / 
